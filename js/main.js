53e20c5cbf22ca7529536fb8f50f9e63f8656fba
Pablo Velaquez <pablocho88@gmail.com>
2019-02-19 09:04:50 -0300

    background color on backToTop + removed old team section

--- js/main.js
+++ js/main.js
@@ -52,6 +52,9 @@
 
 		// Back To Top Appear
 		wScroll > 700 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
+
+		// Section on color
+		wScroll > 1914 ? $('#back-to-top').addClass('green') : $('#back-to-top').removeClass('green');
 	});
 
 	///////////////////////////
